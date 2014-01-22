ae985c5e9fb2b8d232c7b8a9c576872b9b2177f4
Etienne de Maricourt <etienne.demaricourt@gmail.com>
2014-01-22 17:53:15 +0100

    log

--- main.cpp
+++ main.cpp
@@ -163,8 +163,8 @@ int main(int argc, char* argv[])
 			
 			timer = getTimeStamp();
 			
-				u = cont.step(stateest, 0.5, dest);
-				//u = cont.step(stateest, 0.5, 0);
+				//u = cont.step(stateest, 0.5, dest);
+				u = cont.step(stateest, 0.5, 0);
 				
 				piLock(0);
 					valve_command_wished = u;
